eceea204977c851c33417979429a0370170dcffd
khanmaster <eashkhan@hotmail.com>
2020-10-13 14:01:57 +0100

    API for postcode lesson added

--- 01_python_dev/06_OOP/03_library_&_modules_&_packages/pip_request_package/post_code_api.py
+++ 01_python_dev/06_OOP/03_library_&_modules_&_packages/pip_request_package/post_code_api.py
@@ -2,7 +2,7 @@ import requests
 
 url = 'http://api.postcodes.io/postcodes/'
 postcode = 'e147le'
-#postcode = input("please enter your postcode ")
+# postcode = input("please enter your postcode ")
 
 url_arg = url + postcode
 print(url_arg)
@@ -10,11 +10,11 @@ print(url_arg)
 # make request and capture response
 response = requests.get(url_arg)
 
-#print(response)
+# print(response)
 
-#print(type(response))
+# print(type(response))
 # how can we parse the data
-#print(response.json())
+# print(response.json())
 
 # let's store this data into a variable so we can iterate through it
 response_dic = response.json()
@@ -23,10 +23,19 @@ print(type(response_dic))
 # now we know the type is dic and we also know how to iterate through it and get keys
 result_dic = response_dic['result']
 print(result_dic)
+
+# how can get the keys and a specific value of any key
 for keys in result_dic.keys():
-    #print(keys,'the value inside is ', result_dic[keys])
+    print(keys)#,'the value inside is ', result_dic[keys])
 
+# How can we the longitude of our postcodd
+    if keys == 'longitude':
+        print('longitude is ' + str(result_dic['longitude']))
 
-    if keys == 'eastings' and 'northings':
-        print(keys)
+# How can we the latitude of our postcodd
+    if keys == 'latitude':
+        print('latitude is ' + str(result_dic['latitude']))
 
+# How can we the postcode of
+    elif keys == 'postcode':
+        print('postcode is ' + str(result_dic['postcode']))
